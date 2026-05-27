5241170ee0d6822af9fd8e44034c721ee45bceba
swathi <battulaswathi15407@gmail.com>
2026-05-27 10:47:18 +0530

    terraform

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -43,14 +43,3 @@ data "aws_ssm_parameter" "openvpn_sg_id" {
     name = "/${var.project}/${var.environment}/openvpn_sg_id"
 }
 
-data "aws_ssm_parameter" "jenkins_sg_id" {
-    name = "/${var.project}/${var.environment}/jenkins_sg_id"
-}
-
-data "aws_ssm_parameter" "jenkins_agent_sg_id" {
-    name = "/${var.project}/${var.environment}/jenkins_agent_sg_id"
-}
-
-data "aws_ssm_parameter" "sonar_sg_id" {
-    name = "/${var.project}/${var.environment}/sonar_sg_id"
-}
